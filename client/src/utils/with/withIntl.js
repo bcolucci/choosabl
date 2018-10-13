df09b1be1e7f1c731f8c00f3a57de3737317683d
Brice Colucci <brice.colucci@logmein.com>
2018-10-13 10:09:36 -0400

    fix react i18n warning (deprecation)

--- client/src/utils/with/withIntl.js
+++ client/src/utils/with/withIntl.js
@@ -1,5 +1,6 @@
-import { translate } from 'react-i18next'
+import { withNamespaces } from 'react-i18next'
 
 const defaultOpts = ['commons', 'langs']
 
-export default (namespaces = []) => translate([...defaultOpts, ...namespaces])
+export default (namespaces = []) =>
+  withNamespaces([...defaultOpts, ...namespaces])
